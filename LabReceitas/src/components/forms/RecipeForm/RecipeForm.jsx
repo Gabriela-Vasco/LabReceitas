c8e4410ec59f41550262ef803cadfedddcb8561e
Gabriela <gabrielavs1226@gmail.com>
2023-07-17 20:37:02 -0300

    fix: responsividade e checkboxes

--- LabReceitas/src/components/forms/RecipeForm/RecipeForm.jsx
+++ LabReceitas/src/components/forms/RecipeForm/RecipeForm.jsx
@@ -4,35 +4,25 @@ import { nanoid } from 'nanoid'
 import "./RecipeForm.css";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
 
 export default function RecipeForm() {
-    const [recipeData, setRecipeData] = useState({
-        name: "",
-        ingredients: "",
-        instructions: "",
-        image: "",
-        containsDairy: false,
-        containsGluten: false,
-        id: ""
-    });
-    const [resizedImage, setResizedImage] = useState(null);
-    const [recipesArray, setRecipesArray] = useState(JSON.parse(localStorage.getItem("recipes")) || []) ;
-    const [selectedFile, setSelectedFile] = useState(null);
+  const [recipeData, setRecipeData] = useState({
+    name: "",
+    ingredients: "",
+    instructions: "",
+    image: "",
+    containsDairy: false,
+    containsGluten: false,
+    id: ""
+  });
+  const [resizedImage, setResizedImage] = useState(null);
+  const [recipesArray, setRecipesArray] = useState(JSON.parse(localStorage.getItem("recipes")) || []);
+  const [selectedFile, setSelectedFile] = useState(null);
 
-    let recipeWithImage = {
-        name: recipeData.name,
-        ingredients: recipeData.ingredients,    
-        instructions: recipeData.instructions,
-        image: resizedImage,
-        containsDairy: recipeData.containsDairy,
-        containsGluten: recipeData.containsGluten,
-        id: recipeData.id
-    }
-
-    useEffect(() => {
-        localStorage.setItem("recipes", JSON.stringify(recipesArray));
-    }, [recipesArray])
+  useEffect(() => {
+    localStorage.setItem("recipes", JSON.stringify(recipesArray));
+  }, [recipesArray])
 
   const resizeFile = (file) =>
     new Promise((resolve) => {
@@ -50,7 +40,7 @@ export default function RecipeForm() {
       );
     });
 
-    const notify = () => toast("Receita cadastrada com sucesso!");
+  const notify = () => toast("Receita cadastrada com sucesso!");
 
   const onChange = async (event) => {
     try {
@@ -63,125 +53,125 @@ export default function RecipeForm() {
     }
   };
 
-    function handleChange(event){
-        const {name, value, type, checked} = event.target
-        setRecipeData(prevRecipeData => ({
-            ...prevRecipeData,
-            [name]: type === "checkbox" ? checked : value
-        }))
-    }
+  function handleChange(event) {
+    const { name, value, type, checked } = event.target;
 
-    function handleSubmit(event){
-        event.preventDefault()
-        recipeWithImage = {
-            name: recipeData.name,
-            ingredients: recipeData.ingredients,
-            instructions: recipeData.instructions,
-            image: resizedImage,
-            containsDairy: recipeData.containsDairy,
-            containsGluten: recipeData.containsGluten,
-            id: nanoid()
-        }
+    setRecipeData(prevRecipeData => ({
+      ...prevRecipeData,
+      [name]: type === "checkbox" ? checked : value
+    }));
 
-        console.log(recipeWithImage)
-        setRecipesArray(prevRecipesArray => [...prevRecipesArray, recipeWithImage])
+  }
 
+  function handleSubmit(event) {
+    event.preventDefault();
+    const recipeWithImage = {
+      name: recipeData.name,
+      ingredients: recipeData.ingredients,
+      instructions: recipeData.instructions,
+      image: resizedImage,
+      containsDairy: recipeData.containsDairy,
+      containsGluten: recipeData.containsGluten,
+      id: nanoid()
+    };
 
-        setRecipeData({
-            name: "",
-            ingredients: "",
-            instructions: "",
-            containsDairy: false,
-            containsGluten: false,
-        })
-        setResizedImage(null);
-        setSelectedFile(null);
-        notify()
-        window.location.reload()
-    }
+    setRecipesArray(prevRecipesArray => [...prevRecipesArray, recipeWithImage]);
 
-    return(
-        <div>
-            <form onSubmit={handleSubmit} className="recipe-form">
-                <label htmlFor="name" >Nome da Receita:</label>
-                <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    onChange={handleChange}
-                    value={recipeData.name}
-                />
-                <label htmlFor="ingredients">Ingredientes:</label>
-                <textarea 
-                    name="ingredients"
-                    id="ingredients"
-                    onChange={handleChange}
-                    value={recipeData.ingredients}
-                />
-                <label htmlFor="instructions">Modo de preparo:</label>
-                <textarea 
-                    name="instructions"
-                    id="instructions"
-                    onChange={handleChange}
-                    value={recipeData.instructions}
-                />
-                <br/>
-                <label htmlFor="imageUpload" id="uploadLabel">
-                    {selectedFile ? selectedFile.name : "Adicionar Imagem"}
-                </label>
-                <input
-                    type="file"
-                    name="imageUpload"
-                    id="imageUpload"
-                    accept="image/*"
-                    onChange={onChange} 
-                />
-                <br />
-                <label>Restrições:</label>
-                <div className="checkboxes">
-                <input
-                    type="checkbox"
-                    name="containsDairy"
-                    onChange={handleChange}
-                    id="containsDairy"
-                    checked={recipeData.containsDairy}
-                />
-                <label htmlFor="containsDairy">
-                    <svg viewBox="0 0 100 100">
-                        <path class="box" d="M82,89H18c-3.87,0-7-3.13-7-7V18c0-3.87,3.13-7,7-7h64c3.87,0,7,3.13,7,7v64C89,85.87,85.87,89,82,89z"/>
-                        <polyline class="check" points="25.5,53.5 39.5,67.5 72.5,34.5 "/>
-                    </svg>
-                    <span>Lactose</span>
-                </label>
-                </div>
-                <div className="checkboxes">
-                <input
-                    type="checkbox"
-                    name="containsGluten"
-                    id="containsGluten"
-                    onChange={handleChange}
-                    checked={recipeData.containsGluten}
-                />  
-                <label htmlFor="containsGluten">
-                    <svg viewBox="0 0 100 100">
-                        <path class="box" d="M82,89H18c-3.87,0-7-3.13-7-7V18c0-3.87,3.13-7,7-7h64c3.87,0,7,3.13,7,7v64C89,85.87,85.87,89,82,89z"/>
-                        <polyline class="check" points="25.5,53.5 39.5,67.5 72.5,34.5 "/>
-                    </svg>
-                    <span>Glúten</span>
-                </label>
-                </div>
-                <br />
-                <button className="form-button">Enviar</button>
-                <ToastContainer
-                    position="top-center"
-                    autoClose={4000}
-                    hideProgressBar={false}
-                    closeOnClick={false}
-                    rtl={false}
-                    pauseOnHover={false}
-                    theme="light"
-                />
-            </form>
+    setRecipeData({
+      name: "",
+      ingredients: "",
+      instructions: "",
+      containsDairy: false,
+      containsGluten: false,
+    });
+    setResizedImage(null);
+    setSelectedFile(null);
+    notify();
+    window.location.reload()
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit} className="recipe-form">
+        <label htmlFor="name">Nome da Receita:</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={handleChange}
+          value={recipeData.name}
+        />
+        <label htmlFor="ingredients">Ingredientes:</label>
+        <textarea
+          name="ingredients"
+          id="ingredients"
+          onChange={handleChange}
+          value={recipeData.ingredients}
+        />
+        <label htmlFor="instructions">Modo de preparo:</label>
+        <textarea
+          name="instructions"
+          id="instructions"
+          onChange={handleChange}
+          value={recipeData.instructions}
+        />
+        <br />
+        <label htmlFor="imageUpload" id="uploadLabel">
+          {selectedFile ? selectedFile.name : "Adicionar Imagem"}
+        </label>
+        <input
+          type="file"
+          name="imageUpload"
+          id="imageUpload"
+          accept="image/*"
+          onChange={onChange}
+        />
+        <br />
+        <label>Restrições:</label>
+        <div className="checkboxes">
+          <input
+            type="checkbox"
+            name="containsDairy"
+            id="containsDairyCheckbox"
+            onChange={handleChange}
+            checked={recipeData.containsDairy}
+          />
+          <label htmlFor="containsDairyCheckbox">
+            <svg viewBox="0 0 100 100">
+              <path className="box" d="M82,89H18c-3.87,0-7-3.13-7-7V18c0-3.87,3.13-7,7-7h64c3.87,0,7,3.13,7,7v64C89,85.87,85.87,89,82,89z" />
+              <polyline className="check" points="25.5,53.5 39.5,67.5 72.5,34.5 " />
+            </svg>
+            <span>Lactose</span>
+          </label>
         </div>
-    )
+        <div className="checkboxes">
+          <input
+            type="checkbox"
+            name="containsGluten"
+            id="containsGlutenCheckbox"
+            onChange={handleChange}
+            checked={recipeData.containsGluten}
+          />
+          <label htmlFor="containsGlutenCheckbox">
+            <svg viewBox="0 0 100 100">
+              <path className="box" d="M82,89H18c-3.87,0-7-3.13-7-7V18c0-3.87,3.13-7,7-7h64c3.87,0,7,3.13,7,7v64C89,85.87,85.87,89,82,89z" />
+              <polyline className="check" points="25.5,53.5 39.5,67.5 72.5,34.5 " />
+            </svg>
+            <span>Glúten</span>
+          </label>
+        </div>
+        <br />
+        <button className="form-button">Enviar</button>
+        <ToastContainer
+          position="top-center"
+          autoClose={4000}
+          hideProgressBar={false}
+          closeOnClick={false}
+          rtl={false}
+          pauseOnHover={false}
+          theme="light"
+        />
+      </form>
+    </div>
+  )
 }
